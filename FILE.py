646018cd7913df8ac4c2d6ea770c331ab34949d0
MR DARK <105338726+JISAN-404@users.noreply.github.com>
2024-10-07 18:22:46 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -2,6 +2,7 @@ import os,time,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import DUMP
+    try:import DUMP 
+    except:import DUMP
 elif bit=='32bit':
     import DUMP32
